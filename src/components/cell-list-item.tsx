dfca7bdb1f5a6180326acaa5575813fcaa64cda0
kk7259951 <kk7259951@gmail.com>
2022-01-07 11:44:31 -0500

    Added action bar with hover styling and delete, move functionality

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
@@ -10,13 +11,27 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
 
-  (cell.type === 'code') 
-    ? child = <CodeCell cell={cell} />
-    : child = <TextEditor cell={cell} />;
+  if (cell.type === 'code') {
+    /* 
+      React Fragment (<>) allows us to show many different elements inside
+      of it but it will not create any DOM elements that will show up
+      inside of the browser.
+    */
+    child = <> 
+      <div className="action-bar-wrapper">
+        <ActionBar id={cell.id} />
+      </div>
+      <CodeCell cell={cell} />
+    </>
+  } else {
+    child = <>
+      <ActionBar id={cell.id} />
+      <TextEditor cell={cell} />
+    </>
+  }
 
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className="cell-list-item">
       {child}
     </div>
   );
